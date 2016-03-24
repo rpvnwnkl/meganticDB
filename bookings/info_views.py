8abdd49be76f0db9858f862f433285a708b89e34
rpvnwnkl <rpvnwnkl@gmail.com>
2016-03-24 14:29:20 -0400

    added info views

--- bookings/info_views.py
+++ bookings/info_views.py
@@ -1,19 +1,29 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic import ArchiveIndexView
 from django.views.generic import YearArchiveView
 from django.views.generic import MonthArchiveView
 
 from bookings.models import Member
 from bookings.models import Camp
+from bookings.models import Reservation
 from bookings.models import ReservationDetail
 
-class MemberInfoView(ListView):
+class MembersInfoView(ListView):
     model = Member
     context_object_name = 'members_info'
 
-class CampInfoView(ListView):
+class CampsInfoView(ListView):
     model = Camp
     context_object_name = 'camps_info'
+
+class ReservationInfoView(DetailView):
+    model = Reservation
+    template_name = 'bookings/reservation_info.html'
+    context_object_name = 'reservation'
+
+    def get_context_data(self, **kwargs):
+        context = super(ReservationInfoView, self).get_context_data(**kwargs)
+        return context
 
 class CampArchive(ArchiveIndexView):
     
